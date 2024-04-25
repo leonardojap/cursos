149b31246a40985366ec621958da2339fcbb75a2
leonardo <leonardo.alvarado@tektonlabs.com>
2024-04-25 15:26:26 -0400

    feat: add dashboard stadistic

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() type: string = '';
+  @Input() start_date: string = '';
+  @Input() end_date: string = '';
+  @Input() students: boolean = false;
 
+  constructor() {}
+
+  ngOnInit() {
+    if (!this.students) {
+      this.type == 'OFFLINE'
+        ? (this.type = 'Presencial')
+        : (this.type = 'Online');
+    }
+  }
 }
